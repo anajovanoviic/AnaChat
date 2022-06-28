35d0b12bc540fcc8eb454b1d0c70eb9e20dd40b2
anajovanoviic <jovanovic.ana9898@gmail.com>
2022-06-28 09:50:59 +0200

    Threading

--- src/SingletonUserMain.java
+++ src/SingletonUserMain.java
@@ -2,7 +2,7 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SingletonUserMain {
+public class SingletonUserMain{
 
     public static void main(String[] args) {
 
